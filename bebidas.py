b6fc26a85df1df8675e48299f7344a4dd56ea91a
DanAcoVaz <a01736279@tec.mx>
2024-04-03 11:10:52 -0600

    Added alphabetical verification

--- bebidas.py
+++ bebidas.py
@@ -2,6 +2,12 @@ def nuevaBebida(s):
     s.replace(" ", "")
     if(s.find(',') == -1):
         return("Entrada no valida")
+
     if(s[0:s.find(',')] < 2 or s[0:s.find(',')] > 15):
         return("Nombre de bebida invalido")
+
+    if(s.isalpha() == False):
+        return("Nombre de bebida invalido")
+
+    
     return("Bebida agregada")
