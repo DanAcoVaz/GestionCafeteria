f0d4cfeabb850e17290cadf4e05f9382b3d38a26
DanAcoVaz <a01736279@tec.mx>
2024-04-03 11:20:55 -0600

    Fixed verifications with name

--- bebidas.py
+++ bebidas.py
@@ -3,7 +3,7 @@ def nuevaBebida(s):
     if(s.find(',') == -1):
         return("Entrada no valida")
 
-    if(s[0:s.find(',')] < 2 or s[0:s.find(',')] > 15):
+    if(len(s[0:s.find(',') - 1]) < 2 or len(s[0:s.find(',') - 1]) > 15):
         return("Nombre de bebida invalido")
 
     if(s.isalpha() == False):
