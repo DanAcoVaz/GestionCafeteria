420dc6557cea0ef39399a9dabb836ba1964fda76
DanAcoVaz <a01736279@tec.mx>
2024-04-03 11:25:15 -0600

    sum changes

--- bebidas.py
+++ bebidas.py
@@ -1,13 +1,19 @@
 def nuevaBebida(s):
     s.replace(" ", "")
     if(s.find(',') == -1):
-        return("Entrada no valida")
+        return("Entrada no valida.")
 
-    if(len(s[0:s.find(',') - 1]) < 2 or len(s[0:s.find(',') - 1]) > 15):
-        return("Nombre de bebida invalido")
+    if(len(s) == len(s[0:s.find(',')])):
+        return("Entrada no valida. Faltan tamaños")
+
+    if(len(s[0:s.find(',') - 1]) < 2):
+        return("Nombre de bebida invalido. Nombre muy corto")
+
+    if(len(s[0:s.find(',') - 1]) > 15):
+        return("Nombre de bebida invalido. Nombre muy largo")
 
     if(s.isalpha() == False):
-        return("Nombre de bebida invalido")
+        return("Nombre de bebida invalido.")
 
     
     return("Bebida agregada")
