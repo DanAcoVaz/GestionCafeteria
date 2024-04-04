e1650b261d1331571cb29a542203a4804ac26c67
DanAcoVaz <a01736279@tec.mx>
2024-04-04 09:51:53 -0600

    Added fixes to code, input tests and started name tests

--- bebidas.py
+++ bebidas.py
@@ -1,24 +1,29 @@
 def nuevaBebida(s):
     #Quitamos los espacios en blanco
-    s.replace(" ", "")
-    #Nos aseguramos de que haya , en el string
-    if(s.find(',') == -1):
-        return(False)
+    s = s.replace(" ", "")
 
+    #Nos aseguramos que haya algo en el string
+    if(len(s) <= 0):
+        return (False)
+    
+    #Nos aseguramos de que haya , en el string
+    if(s.count(',') <= 0):
+        return(False)
+    
     #Nos aseguramos que el string no acabe en ,
     if(s[-1] == ','):
         return(False)
 
     #Nos aseguramos que el nombre sea mayor a 2 caracteres
-    if(len(s[0:s.find(',') - 1]) < 2):
+    if(len(s[0:s.find(',')]) < 2):
         return(False)
 
     #Nos aseguramos que el nombre sea menor a 15 caracteres
-    if(len(s[0:s.find(',') - 1]) > 15):
+    if(len(s[0:s.find(',')]) > 15):
         return(False)
 
     #Nos aseguramos que el nombre sea completamente alfabetico
-    if(s.isalpha() == False):
+    if(s[0:s.find(',')].isalpha() == False):
         return(False)
 
     #contamos cuantas , hay en el string
@@ -37,7 +42,7 @@ def nuevaBebida(s):
     for i in range(commaCount):
         #Checamos si este es el ultimo tamaño
         if(commaCount == (i + 1)):
-            number = int(s[index + 1])
+            number = int(s[index + 1:])
             #Checamos si el tamaño esta dentro del rango
             if(number < 1 or number > 48):
                 return(False)
--- tests/first_test.py
+++ tests/first_test.py
@@ -1,11 +1,35 @@
 from bebidas import nuevaBebida
 
+#Test para probar diferentes inputs
 def testInputs():
+    #Input vacio
     assert nuevaBebida("") == False
+    #Input acabando en , sin tamaños
     assert nuevaBebida("SuperJugo,") == False
+    #Input sin nombre
     assert nuevaBebida(",56") == False
+    #Solo una , en el input
     assert nuevaBebida(",") == False
+    #Tamaños primero y luego nombre
     assert nuevaBebida("3, 7, 9, Pepsi") == False
+    #Solo nombre
     assert nuevaBebida("Coca") == False
-    assert nuevaBebida("Pepsi, 5, 7, 10, 12, 15, 18, 22") == False
-    assert nuevaBebida("     Manzanita Sol,   3,    6,    12") == False
+    #El input acaba en ,
+    assert nuevaBebida("Pepsi, 5, 7, 10, 12,") == False
+    #Input correcto con muchos espacios (para probar que los espacios se ignoran)
+    assert nuevaBebida("    Manzanita Sol,   3,    6,    12") == True
+    #Input correcto
+    assert nuevaBebida("Pepsi, 10, 20, 30") == True
+
+#Tests para probar la introduccion del nombre
+def testNames():
+    #Nombre de menos de 2 caracteres
+    assert nuevaBebida("K, 2, 4, 6, 8") == False
+    #Nombre de 2 caracteres
+    assert nuevaBebida("KO, 3, 5, 8") == True
+    #Nombre de muchos caracteres (>15)
+    assert nuevaBebida("The Super powerful elixir of the gods, 1, 2, 3, 4") == False
+    #Nombre de 16 caracteres
+    assert nuevaBebida("Powerful Beverage, 10, 20, 30") == False
+    #Nombre de 15 caracteres
+    assert nuevaBebida("Powerful Elixirs, 10, 15, 20, 25, 30") == True
